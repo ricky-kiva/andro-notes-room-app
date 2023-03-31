870df29d313748768c5c9d0c0f73548fe9e06706
ricky-kiva <ricky.kiva@gmail.com>
2023-03-31 14:30:24 +0700

    add Note Adapter

---
+++ app/src/main/java/com/rickyslash/noteroomapp/helper/NoteDiffCallback.kt
@@ -0,0 +1,25 @@
+package com.rickyslash.noteroomapp.helper
+
+import androidx.recyclerview.widget.DiffUtil
+import com.rickyslash.noteroomapp.database.Note
+
+class NoteDiffCallback(private val mOldNoteList: List<Note>, private val mNewNoteList: List<Note>): DiffUtil.Callback() {
+    override fun getOldListSize(): Int {
+        return mOldNoteList.size
+    }
+
+    override fun getNewListSize(): Int {
+        return mNewNoteList.size
+    }
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return mOldNoteList[oldItemPosition].id == mNewNoteList[newItemPosition].id
+    }
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        val oldEmployee = mOldNoteList[oldItemPosition]
+        val newEmployee = mNewNoteList[newItemPosition]
+        return oldEmployee.title == newEmployee.title && oldEmployee.desc == newEmployee.desc
+    }
+
+}

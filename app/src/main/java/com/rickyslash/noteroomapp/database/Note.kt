4ae648e199858cccfbaec428fd95e01b95e24d98
ricky-kiva <ricky.kiva@gmail.com>
2023-03-31 14:27:56 +0700

    add DDL

---
+++ app/src/main/java/com/rickyslash/noteroomapp/database/Note.kt
@@ -0,0 +1,24 @@
+package com.rickyslash.noteroomapp.database
+
+import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+@Entity
+data class Note(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    var id: Int = 0,
+
+    @ColumnInfo(name = "title")
+    var title: String? = null,
+
+    @ColumnInfo(name = "description")
+    var desc: String? = null,
+
+    @ColumnInfo(name = "date")
+    var date: String? = null
+): Parcelable

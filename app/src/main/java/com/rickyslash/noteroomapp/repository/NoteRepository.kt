a17a244378b263bd84cbd5eb392f9ecb2ecbbef6
ricky-kiva <ricky.kiva@gmail.com>
2023-03-31 14:29:41 +0700

    add Note Repo

---
+++ app/src/main/java/com/rickyslash/noteroomapp/repository/NoteRepository.kt
@@ -0,0 +1,34 @@
+package com.rickyslash.noteroomapp.repository
+
+import android.app.Application
+import androidx.lifecycle.LiveData
+import com.rickyslash.noteroomapp.database.Note
+import com.rickyslash.noteroomapp.database.NoteDao
+import com.rickyslash.noteroomapp.database.NoteRoomDatabase
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+class NoteRepository(application: Application) {
+    private val mNotesDao: NoteDao
+    private val executorService: ExecutorService = Executors.newSingleThreadExecutor()
+
+    init {
+        val db = NoteRoomDatabase.getDatabase(application)
+        mNotesDao = db.noteDao()
+    }
+
+    fun getAllNotes(): LiveData<List<Note>> = mNotesDao.getAllNotes()
+
+    fun insert(note: Note) {
+        executorService.execute { mNotesDao.insert(note) }
+    }
+
+    fun update(note: Note) {
+        executorService.execute { mNotesDao.update(note) }
+    }
+
+    fun delete(note: Note) {
+        executorService.execute { mNotesDao.delete(note) }
+    }
+
+}
